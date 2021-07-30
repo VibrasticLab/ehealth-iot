93a09df79dcecfc5c4e50911359b9aadcbe5f886
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-07-30 21:02:34 +0700

    bluez tkinter doc

--- bttkgui/bttkgui.py
+++ bttkgui/bttkgui.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""@package blueztk
+Bluetoothctl GUI using tkinter
+"""
+
+# Imports
 import tkinter as tk
 import subprocess as sp
 
-def memfree():
+def btlist():
+    """Bluetooth List Routine
+    """
     txtbox.delete('1.0',tk.END)
     mfree = sp.run(["bluetoothctl","--timeout","5","scan","on"],stdout=sp.PIPE,stderr=sp.PIPE).stdout.decode("utf-8")
     txtbox.insert(tk.END,mfree)
 
 sp.run(["bluetoothctl","power","on"],stdout=sp.PIPE,stderr=sp.PIPE)
 
+## Main Window
 window = tk.Tk()
 window.geometry("480x320")
 window.title("Tk MemFree")
 
+## Text Box
 txtbox = tk.Text(window,height=15,width=80)
-txtbox.config(font=("Liberation Mono",8))
 txtbox.pack()
 
-btnmem = tk.Button(window,text="Bluetooth List",command=memfree)
-btnmem.pack()
+## Text Box Font
+txtbox.config(font=("Liberation Mono",8))
+
+## Button List
+btnbtlist = tk.Button(window,text="Bluetooth List",command=btlist)
+btnbtlist.pack()
 
 window.mainloop()
