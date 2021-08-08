567e9a52b22d6daa9ff12d8f94f9e404591089c9
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-08-08 19:30:47 +0700

    cough gui

--- bttkgui/coughgui.py
+++ bttkgui/coughgui.py
@@ -12,6 +12,13 @@ import tkinter as tk
 import subprocess as sp
 from tkinter import font
 
+# Import matplotlib libraries
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
+
 class BtTk():
     """CoughAnalyzer Program with GUI
     """
@@ -32,44 +39,46 @@ class BtTk():
         self.lbltitle = tk.Label(self.window, text="Cough Analyzer Program")
         self.lbltitle.pack(side=tk.TOP)
 
-        # Status Label
-        self.lblstatus = tk.Label(self.window, text="Program Started")
-        self.lblstatus.pack(side=tk.BOTTOM)
-
-        # Graph Box Frame
-        self.graphfrm = tk.Frame(self.window)
-
-        # Pack Listbox Frame
-        self.graphfrm.pack(side=tk.LEFT, expand=True)
-
         # Button Frame
         self.btnfrm = tk.Frame(self.window)
 
         # Button Start to Analyze
         self.btnstart = tk.Button(self.btnfrm, text="  Start ")
-        self.btnstart.pack()
-
-        # Button Stop to Analyze
-        self.btnstop = tk.Button(self.btnfrm, text="  Stop  ")
-        self.btnstop.pack()
+        self.btnstart.pack(side=tk.LEFT)
 
         # Button Graph Test
         self.btntest = tk.Button(self.btnfrm, text="  Test  ")
-        self.btntest.pack()
+        self.btntest.pack(side=tk.LEFT)
 
         # Button App Quit
         self.btnquit = tk.Button(self.btnfrm, text="  Quit  ", command=self.appquit)
-        self.btnquit.pack()
+        self.btnquit.pack(side=tk.LEFT)
 
         # Pack Button Frame
-        self.btnfrm.pack(side=tk.RIGHT, expand=True)
+        self.btnfrm.pack(side=tk.TOP)
+
+        # Graph Frame
+        self.graphfrm = tk.Frame() 
+
+        # Example Figure Plot
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        t = np.arange(0, 3, .01)
+        self.fig.add_subplot(111).plot(t, 2 * np.sin(20 * np.pi * t))
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.graphfrm)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.LEFT)
+
+        self.canvas.mpl_connect("key_press_event", self.on_key_press)
+
+        # Pack Graph Frame
+        # Must packed the last
+        self.graphfrm.pack(side=tk.BOTTOM,expand=True)
 
         # Button Font
-        btnfont = font.Font(self.btnfrm, family="Liberation Mono", size=14)
+        btnfont = font.Font(self.btnfrm, family="Liberation Mono", size=10)
         self.lbltitle.config(font=btnfont)
-        self.lblstatus.config(font=btnfont)
         self.btnstart.config(font=btnfont)
-        self.btnstop.config(font=btnfont)
         self.btntest.config(font=btnfont)
         self.btnquit.config(font=btnfont)
 
@@ -77,11 +86,7 @@ class BtTk():
         if self.DarkTheme:
             self.window.config(bg="black")
             self.lbltitle.config(bg='black', fg='white')
-            self.lblstatus.config(bg='black', fg='white')
-            self.graphfrm.config(bg='black')
-            self.lstbox.config(bg='black', fg='white')
             self.btnstart.config(bg='black', fg='white')
-            self.btnstop.config(bg='black', fg='white')
             self.btntest.config(bg='black', fg='white')
             self.btnquit.config(bg='black', fg='white')
             self.btnfrm.config(bg='black')
@@ -99,6 +104,11 @@ class BtTk():
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
         return False
+
+    def on_key_press(self,event):
+        """ Test Graph Key Event"""
+
+        key_press_handler(event, self.canvas)
 
     def appquit(self):
         """ Quit Program"""
