8d31d2f3c1d7bf0c36d000e01c1db7c983ea807d
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-08-20 19:46:11 +0700

    time latency in question

--- coughgui/alsarecnumpy.py
+++ coughgui/alsarecnumpy.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import alsaaudio as alsa
+
 device = 'dmic_sv'
 file = open('out.raw', 'wb')
+
 rawinput = alsa.PCM(alsa.PCM_CAPTURE, alsa.PCM_NORMAL,channels=2, rate=44100, format=alsa.PCM_FORMAT_S16_LE,periodsize=512, device=device)
 long, data = rawinput.read()
+decoded = np.frombuffer(data, dtype='i2' )
+print('Array PCM: \n',decoded)
+
 file.write(data)
 file.flush()
 file.close()
+
+
--- coughgui/coughgui.py
+++ coughgui/coughgui.py
@@ -23,13 +23,17 @@ from time import sleep
 from threading import Timer as tmr
 from threading import Thread as thd
 
+import alsaaudio as alsa
+
 class CoughTk():
     """CoughAnalyzer Program with GUI
     """
 
-    loopdata = False
     loopgraph = False
+    Record = False
+    RecLoop = True
     DarkTheme = False
+    AudioLong = 512
 
     def __init__(self):
         super(CoughTk, self).__init__()
@@ -47,15 +51,11 @@ class CoughTk():
         self.btnfrm = tk.Frame(self.window)
 
         # Button Start to Analyze
-        self.btnstart = tk.Button(self.btnfrm, text=" Start/Stop", command=self.randomtest)
+        self.btnstart = tk.Button(self.btnfrm, text="Start", command=self.recstart)
         self.btnstart.pack(side=tk.LEFT)
 
-        # Button Graph Test
-        self.btntest = tk.Button(self.btnfrm, text="  Test  ")
-        self.btntest.pack(side=tk.LEFT)
-
         # Button App Quit
-        self.btnquit = tk.Button(self.btnfrm, text="  Quit  ", command=self.appquit)
+        self.btnquit = tk.Button(self.btnfrm, text="Quit", command=self.appquit)
         self.btnquit.pack(side=tk.LEFT)
 
         # Pack Button Frame
@@ -65,22 +65,21 @@ class CoughTk():
         self.graphfrm = tk.Frame()
 
         # Graph Data
-        self.F = 5
-        self.t = np.arange(0, 3, .01)
-        self.y = np.sin(self.F * np.pi * self.t)
+        self.X = np.arange(0, 2*self.AudioLong, 1)
+        self.Y = np.zeros(2*self.AudioLong, dtype='i2')
 
         # Example Figure Plot
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.ax.set_ylim(-1,1)
-        self.line, = self.ax.plot(self.t, self.y)
+        self.line, = self.ax.plot(self.X, self.Y)
 
         style.use('ggplot')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.graphfrm)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.LEFT)
 
-        self.canvas.mpl_connect("key_press_event", self.on_key_press)
+        #self.canvas.mpl_connect("key_press_event", self.on_key_press)
 
         # Pack Graph Frame
         # Must packed the last
@@ -90,7 +89,6 @@ class CoughTk():
         btnfont = font.Font(self.btnfrm, family="Liberation Mono", size=15)
         self.lbltitle.config(font=btnfont)
         self.btnstart.config(font=btnfont)
-        self.btntest.config(font=btnfont)
         self.btnquit.config(font=btnfont)
 
         # Dark Theme Config
@@ -98,13 +96,18 @@ class CoughTk():
             self.window.config(bg="black")
             self.lbltitle.config(bg='black', fg='white')
             self.btnstart.config(bg='black', fg='white')
-            self.btntest.config(bg='black', fg='white')
             self.btnquit.config(bg='black', fg='white')
             self.btnfrm.config(bg='black')
 
         # start graph loop
         self.loopgraph = True
         thd(target=self.graphloop).start()
+
+        # start mic routine
+        device = 'dmic_sv'
+        self.file = open('out.raw', 'wb')
+        self.rawinput = alsa.PCM(alsa.PCM_CAPTURE, alsa.PCM_NORMAL, channels=2, rate=44100,format=alsa.PCM_FORMAT_S16_LE, periodsize=self.AudioLong, device=device)
+        thd(target=self.recprocess).start()
 
         # Main Loop
         self.window.mainloop()
@@ -114,42 +117,36 @@ class CoughTk():
 
         key_press_handler(event, self.canvas)
 
-    def appquit(self):
-        """ Quit Program"""
+    def recstart(self):
+        if self.Record:
+            self.Record = False
+            self.btnstart.config(text='Start')
+        else:
+            self.Record = True
+            self.btnstart.config(text='Stop')
 
-        self.loopdata = False
+    def appquit(self):
+        self.Record = False
+        self.RecLoop = False
         self.loopgraph = False
         self.window.destroy()
 
-    def randomtest(self):
-        if self.loopdata:
-            self.loopdata = False
-        else:
-            self.loopdata = True
-            self.randomfreq()
+    def recprocess(self):
+        while self.RecLoop:
+            if self.Record:
+                long, indata = self.rawinput.read()
 
-    def randomfreq(self):
-        """ Update Data with random frequency"""
-
-        self.F = rnd.randint(5,20)
-        self.y = np.sin(self.F* np.pi * self.t)
-        print("update")
-
-        if self.loopdata:
-            tmr(0.1,self.randomfreq).start()
+                if long:
+                    self.Y = np.frombuffer(indata, dtype='i2' ) / 32768
+                    sleep(0.01)
 
     def graphloop(self):
         """ Refresh Plot using new data"""
 
         while self.loopgraph:
-            self.graphdraw()
+            self.line.set_data(self.X,self.Y)
+            self.canvas.draw_idle()
             sleep(0.005)
-
-    def graphdraw(self):
-        """ Refresh Plot using new data"""
-
-        self.line.set_data(self.t,self.y)
-        self.canvas.draw_idle()
 
 if __name__ == "__main__":
     cough = CoughTk()
