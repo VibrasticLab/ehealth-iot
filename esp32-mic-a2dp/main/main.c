382db272d4e74332a49afa047c9e71803b32d7c4
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-09-13 08:02:17 +0700

    main.c with min task heap 512

--- esp32-mic-a2dp/main/main.c
+++ esp32-mic-a2dp/main/main.c
@@ -7,12 +7,15 @@
 #include "sdkconfig.h"
 
 #define BLINK_GPIO  2
-#define BLINK_DELAY 100
+#define BLINK_DELAY 100 
 
-void hello_task(void *pvParameter){
+static void led_task(void *pvParameter){
     while (1) {
-        printf("Hello World\n");
-        vTaskDelay(5*BLINK_DELAY / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
     }
 }
 
@@ -20,13 +23,10 @@ void app_main(void){
     gpio_reset_pin(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-    xTaskCreate(&hello_task, "helloworld", 1024, NULL, 5, NULL);
+    xTaskCreate(&led_task, "led_task", 512, NULL, 5, NULL);
 
+    printf("System Initiated\n");
     while(1){
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
-
-        gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
     }
 }
