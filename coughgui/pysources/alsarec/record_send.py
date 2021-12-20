84b60c2e799942a8f82f66f20bd6a37f7d51f3b3
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-12-20 14:15:32 +0700

    send as wav

--- coughgui/pysources/alsarec/record_send.py
+++ coughgui/pysources/alsarec/record_send.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import wave
 import requests
 
-files = {'file_batuk': open("out.raw","rb")}
-values = {'nama': 'pasien', 'gender': 'unknown', 'umur': 0}
-req = requests.post("http://103.147.32.57/api/device/sendData/303",files=files, data=values)
-#req = requests.post("http://10.124.5.198/api/device/sendData/303",files=files, data=values)
+rawrecfile = "out.raw"
+wavrecfile = "out.wav"
+
+def sendrecord():
+    with open(rawrecfile,"rb") as in_raw:
+        rawdata = in_raw.read()
+        with wave.open(wavrecfile,"wb") as out_wav:
+            out_wav.setparams((2, 2, 44100, 0, 'NONE', 'NONE'))
+            out_wav.writeframesraw(rawdata)
+
+    files = {'file_batuk': open(rawrecfile,"rb")}
+    values = {'nama': 'pasien', 'gender': 'unknown', 'umur': 0}
+    requests.post("http://10.124.5.198/api/device/sendData/303",files=files, data=values)
+    #requests.post("http://103.147.32.57/api/device/sendData/303",files=files, data=values)
+
+if __name__ == "__main__":
+    sendrecord()
+
